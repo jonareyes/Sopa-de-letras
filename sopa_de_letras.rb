baa3081861b9be6a80261beefb4fa43b2a89e5f0
codeamac08 <codeamac08@gmail.com>
2015-10-16 18:57:28 -0500

    Se agrega revision de reversa y de adelante para atras en la sopa de letras

--- sopa_de_letras.rb
+++ sopa_de_letras.rb
@@ -2,9 +2,9 @@ class Board
 # You should put here the given boards templates
 	
   def initialize
-  	mix = "POEMAXCXXXXAXXSXNXXCMXDXIXXNROXXOXNXXR".gsub(/[X]/){|x| ("A".."Z").to_a.sample}
+  	mix = "AMEOPXCXXXXAXXSXNXXCMXDXIXXNROXXOXNXXR".gsub(/[X]/){|x| ("A".."Z").to_a.sample}
   	@board =  mix.scan(/.{5}/).map!{|row| row.split("")} 
-  	@board 
+  	 
    
   end
 
@@ -18,7 +18,30 @@ class Board
   	end	
   	" "
   end
+
+  def include?(word)
+
+    # word_in_array(x,word) => renglon 
+    # word_in_array(x,word) => renglon_en_reversa
+    # word_in_array(x,word) => columna
+    # word_in_array(x,word) => columna_en_reversa
+  end
+
+  def check_rows(word)
+    @board.each do |row|
+      return true if word_in_array(row,word.upcase)  
+      return true if word_in_array(row.join.reverse.split(""),word.upcase)  
+    end
+    false
+  end
+
+  def word_in_array(array,word)
+    array.join.include?(word) ? true : false
+  end
 end
+
 
 board = Board.new
 puts board
+puts board.check_rows("poema")
+puts board.check_rows("javier")

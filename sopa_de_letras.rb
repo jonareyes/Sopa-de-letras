1327df17cac760d188b2aa03dd29811fb4a0ff39
codeamac08 <codeamac08@gmail.com>
2015-10-21 19:40:45 -0500

    Se termina ejercicio de sopa de letras haciendo que lea de adelante atras, atras adelante, arriba abajo, abajo arriba, y en diagonales

--- sopa_de_letras.rb
+++ sopa_de_letras.rb
@@ -9,8 +9,11 @@ class Board
   end
 
 
-  def to_s
-    @board.each do |row|
+  def to_s(board=nil)
+    if board.nil?
+        board = @board
+    end
+    board.each do |row|
       row.each do |element|
         print "  #{element}  "
       end
@@ -19,14 +22,25 @@ class Board
     " "
   end
 
-
   def check_board(word)
-    convert_diagonals_to_cols(@board).transpose
-    # if check_rows(@board, word) || check_rows(@board.transpose, word) || 
-    #   true
-    # else
-    #   false
-    # end
+    new_board = []
+    @board.map{|row| new_board << row.dup}
+    diagonal = convert_diagonals_to_cols(new_board)
+    # p "normal"
+    # to_s(@board)
+    # p "normal transpuesto"
+    # to_s(@board.transpose)
+    # p "diagonal"
+    # to_s(diagonal)
+    # p "diagonal transpuesto"
+    # to_s(diagonal.transpose)
+
+
+    if check_rows(@board, word) || check_rows(@board.transpose, word) || check_rows(diagonal, word) || check_rows(diagonal.transpose, word) 
+      true
+    else
+      false
+    end
   end
 
   def check_rows(board, word)
@@ -37,14 +51,16 @@ class Board
     false
   end
 
-   def convert_diagonals_to_cols(board)
-    board.each_with_index do |row, index|
+  def convert_diagonals_to_cols(board_diagonal)
+    board_diagonal.each_with_index do |row, index|
       index.times do  
-        row.unshift(" ")  
+        row.unshift(" ")
       end
-      p row
+      (board_diagonal.length-index).times do 
+        row.push(" ")
+      end
     end
-    board
+    board_diagonal
   end
   
 
@@ -54,22 +70,23 @@ class Board
 end
 
 
-# words = ["POEMA", "CANCION", "RONDAS", "RIMAS"]  
-# board = Board.new("POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR")
-# puts board
-# words.each do |word|
-#  p board.check_board(word)
-# end
+words = ["POEMA", "CANCION", "RONDAS", "RIMAS", "SAMIR"]  
+board = Board.new("POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR")
+puts board
+words.each do |word|
+ p board.check_board(word)
+end
 
-# words1 = ["MANGO", "SANDIA", "MELON", "PLATANO"]
-# board1 = Board.new("XXXXPXXXXLXAMXAXIEXTXDLXAXNOXNMANGOXSXXX")
-# puts board1
-# words1.each do |word|
-#   p board1.check_board(word) 
-# end
-words2 = ["ROJO", "VERDE", "AZUL", "NEGRO"]
+words1 = ["MANGO", "SANDIA", "MELON", "PLATANO"]
+board1 = Board.new("XXXXPXXXXLXAMXAXIEXTXDLXAXNOXNMANGOXSXXX")
+puts board1
+words1.each do |word|
+  p board1.check_board(word) 
+end
+
+words2 = ["ROJO", "VERDE", "AZUL", "NEGRO" ]
 board2 = Board.new("OJORXXXXXXXEXXOXDXRXXRGLXXEXUXNVXZXXXXAX")
 puts board2
-p board2.check_board("poema")
-
-#board2.check_diagonal()
+words2.each do |word|
+  p board2.check_board(word) 
+end
